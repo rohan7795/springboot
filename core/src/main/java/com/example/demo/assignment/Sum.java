b356dda164e7d231e6c3550e65a9b0d10cc7db18
PERFICIENT\rohan.battulwar <rohan.battulwar@perficient.com>
2022-01-18 19:18:22 +0530

    Missed

---
+++ core/src/main/java/com/example/demo/assignment/Sum.java
@@ -0,0 +1,12 @@
+package com.example.demo.assignment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sum {
+
+	public int TwoSum(int a, int b) {
+		return a + b;
+	}
+
+}
