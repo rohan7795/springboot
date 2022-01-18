d550e8ebbbdbe463bcf7b681ba19e7cd0ef9412c
PERFICIENT\rohan.battulwar <rohan.battulwar@perficient.com>
2022-01-18 17:43:51 +0530

    remaining

--- core/src/test/java/com/example/demo/CoreApplicationTests.java
+++ core/src/test/java/com/example/demo/CoreApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.service.ServiceImpl;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CoreApplicationTests {
+	
+	@Autowired
+	ServiceImpl service;
 
 	@Test
-	void contextLoads() {
+	void testDi() {
+		assertNotNull(service);
+	
+		
 	}
 
 }
