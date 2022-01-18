f9780796b986b3b206ee8b66015d11df4ab6b826
PERFICIENT\rohan.battulwar <rohan.battulwar@perficient.com>
2022-01-18 19:17:21 +0530

    SpringDataJpa Example

--- core/src/test/java/com/example/demo/CoreApplicationTests.java
+++ core/src/test/java/com/example/demo/CoreApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.assignment.Sum;
 import com.example.demo.service.ServiceImpl;
 
 import static org.junit.Assert.*;
@@ -16,11 +17,20 @@ class CoreApplicationTests {
 	
 	@Autowired
 	ServiceImpl service;
+	
+	@Autowired
+	Sum s;
 
 	@Test
 	void testDi() {
 		assertNotNull(service);
 	
+		
+	}
+	@Test
+	void testadd() {
+		
+		assertEquals(s.TwoSum(3, 4), 7);
 		
 	}
 
---
+++ springbootdatajpa/src/test/java/com/example/demo/SpringbootdatajpaApplicationTests.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.security.interfaces.RSAKey;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.entity.Student;
+import com.example.demo.entity.repo.StudentRepository;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringbootdatajpaApplicationTests {
+
+	@Autowired
+	private StudentRepository sr;
+
+	@Test
+	void testSaveStudent() {
+		Student s = new Student();
+		s.setId(12);
+		s.setName("ROhan");
+		s.setTestScore(100);
+		sr.save(s);
+
+		Student student = sr.findById(12l).get();
+
+		assertNotNull(student);
+	}
+
+}
