e6732f613d9f78931890d4293d006446de70a4f5
PERFICIENT\rohan.battulwar <rohan.battulwar@perficient.com>
2022-01-19 19:31:32 +0530

    consume rest service test

--- productrestapi/src/test/java/com/springweb/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/springweb/ProductrestapiApplicationTests.java
@@ -1,13 +1,55 @@
 package com.springweb;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+import com.springweb.entity.Product;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ProductrestapiApplicationTests {
 
+	@Autowired
+	RestTemplate restT;
+
 	@Test
-	void contextLoads() {
+	void getProduct() {
+		Product product = restT.getForObject("http://localhost:8080/rohanapp/products/11", Product.class);
+
+		assertNotNull(product);
+		assertEquals(product.getName(), "nestle");
+	}
+	
+//	@Test
+	void createProduct() {
+		
+		Product product = new Product();
+		product.setName("nestle");
+		product.setDescription("mil product");
+		product.setPrice(90);
+		Product newProduct = restT.postForObject("http://localhost:8080/rohanapp/products/", product, Product.class);
+		
+		
+
+		assertNotNull(newProduct);
+		assertEquals(product.getName(), "nestle");
+	}
+	
+	@Test
+	void updateProduct() {
+		Product product = restT.getForObject("http://localhost:8080/rohanapp/products/11", Product.class);
+		
+		product.setPrice(10000000);
+//		String pr = "http://localhost:8080/rohanapp/products/"+product.getId();
+		restT.put("http://localhost:8080/rohanapp/products/",product);
+		
+	
 	}
 
 }
